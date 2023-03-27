ced45cc191beb9b27db17fdd211d7b63073e4958
Olivia <zoe.i2k1@gmail.com>
2023-03-27 23:18:51 +0200

    Add utility header for importing all teh structz

---
+++ src/stackapi/data/structs/ImportAll.hpp
@@ -0,0 +1,25 @@
+#include "stackapi/data/structs/collectives/Collective.hpp"
+#include "stackapi/data/structs/collectives/CollectiveExternalLink.hpp"
+#include "stackapi/data/structs/collectives/CollectiveMembership.hpp"
+#include "stackapi/data/structs/collectives/CollectiveRecommendation.hpp"
+
+#include "stackapi/data/structs/comments/Comment.hpp"
+
+#include "stackapi/data/structs/network/RelatedSite.hpp"
+#include "stackapi/data/structs/network/Site.hpp"
+#include "stackapi/data/structs/network/Styling.hpp"
+
+#include "stackapi/data/structs/posts/Answer.hpp"
+#include "stackapi/data/structs/posts/ClosedDetails.hpp"
+#include "stackapi/data/structs/posts/MigrationInfo.hpp"
+#include "stackapi/data/structs/posts/Notice.hpp"
+#include "stackapi/data/structs/posts/OriginalQuestion.hpp"
+#include "stackapi/data/structs/posts/Post.hpp"
+#include "stackapi/data/structs/posts/Question.hpp"
+
+#include "stackapi/data/structs/users/BadgeCount.hpp"
+#include "stackapi/data/structs/users/ShallowUser.hpp"
+#include "stackapi/data/structs/users/User.hpp"
+
+#include "stackapi/data/structs/APIResponse.hpp"
+#include "stackapi/data/structs/NullStruct.hpp"
