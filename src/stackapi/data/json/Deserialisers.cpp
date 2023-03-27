64cba63a583034df86a237de603cedce2ac0d8d4
Olivia <zoe.i2k1@gmail.com>
2023-03-27 23:19:44 +0200

    Make the deserialisation file use ImportAll instead
    Should help force me keep ImportAll up to date

--- src/stackapi/data/json/Deserialisers.cpp
+++ src/stackapi/data/json/Deserialisers.cpp
@@ -1,24 +1,6 @@
 #include <nlohmann/json.hpp>
 
-#include "stackapi/data/structs/posts/ClosedDetails.hpp"
-#include "stackapi/data/structs/posts/Notice.hpp"
-#include "stackapi/data/structs/posts/OriginalQuestion.hpp"
-#include "stackapi/data/structs/posts/Post.hpp"
-#include "stackapi/data/structs/posts/Question.hpp"
-#include "stackapi/data/structs/users/BadgeCount.hpp"
-#include "stackapi/data/structs/comments/Comment.hpp"
-#include "stackapi/data/structs/NullStruct.hpp"
-#include "stackapi/data/structs/users/ShallowUser.hpp"
-#include "stackapi/data/structs/users/User.hpp"
-#include "stackapi/data/structs/collectives/Collective.hpp"
-#include "stackapi/data/structs/collectives/CollectiveExternalLink.hpp"
-#include "stackapi/data/structs/collectives/CollectiveMembership.hpp"
-#include "stackapi/data/structs/collectives/CollectiveRecommendation.hpp"
-#include "stackapi/data/structs/network/RelatedSite.hpp"
-#include "stackapi/data/structs/network/Site.hpp"
-#include "stackapi/data/structs/network/Styling.hpp"
-#include "stackapi/data/structs/posts/Answer.hpp"
-#include "stackapi/data/structs/posts/MigrationInfo.hpp"
+#include "stackapi/data/structs/ImportAll.hpp"
 
 
 #define COND_AT(field, dest) if (j.contains(field)) { \
